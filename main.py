826a68178ef2938d575b0cd0a4b01a3c13d1be81
Thekiddo7 <therealkiddo7@gmail.com>
2022-02-23 22:55:01 -0500

    Create main.py
    Created simple 1-100 number guesser bot

---
+++ main.py
@@ -0,0 +1,36 @@
+guess = 50
+low = 50
+high = 50
+count = 0
+hlc = ""
+
+print("Think of a random number from 1 to 100, and I will to try guess it.")
+
+while hlc != "same":
+  hlc = input("I guess " + str(guess) + ", is the answer higher, lower, or the same? ")
+
+  if hlc == "higher":
+    low = guess
+
+    if high == 50 & low == 50:
+      high = 100
+      
+    guess = round((high-low) / 2 + low)
+    
+  if hlc == "lower":
+    high = guess
+
+    if high == 50 & low == 50:
+      low = 0
+    
+    guess = round(high - (high-low) / 2)
+
+  count += 1
+
+  print(high)
+  print(low)
+  
+if count == 1:
+  print("I got it in 1 try! I guess I am the best bot :3")
+else:
+  print("I got it in " + str(count) + " tries! That's worse than I expected...")
